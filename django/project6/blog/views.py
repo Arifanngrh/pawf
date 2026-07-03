8b1ed0773053ee0a83cfd5199b20a73b06b6a3ab
Arifan Nugraha <arifanngrh1@gmail.com>
2026-07-03 23:20:25 +0700

    update tugas

--- django/project6/blog/views.py
+++ django/project6/blog/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import get_object_or_404, render # update
-
+from django.db.models import Q
 from .models import Post
 
 
 def post_list(request):
     posts = Post.objects.all()
+    
+    # Search functionality
+    search_query = request.GET.get('q', '')
+    if search_query:
+        posts = posts.filter(
+            Q(title__icontains=search_query) | 
+            Q(body__icontains=search_query) |
+            Q(author__username__icontains=search_query)
+        )
+    
+    # Sorting by latest first
+    posts = posts.order_by('-id')
+    
     return render(request, "home.html", {"posts": posts})
 
 # add
